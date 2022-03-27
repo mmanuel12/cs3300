5f9d4207b0bc9fc1810bb97b98fa8c05589da671
mmanuel12 <mmanuel@uccs.edu>
2022-03-27 14:04:46 +0000

    Update Heroku code

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -5,5 +5,15 @@ RSpec.feature "Visiting the homepage", type: :feature do
     visit root_path
     expect(page).to have_text("Projects")
   end
+
+  scenario "The visitor should see project title" do
+    visit root_path
+    expect(page).to have_text("Title")
+  end
+
+  scenario "The visitor should see project description" do
+    visit root_path
+    expect(page).to have_text("Description")
+  end
 end
 
