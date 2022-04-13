cf0b39a7fae9941b4d877a9761dc4055eb24b3ff
mmanuel12 <mmanuel@uccs.edu>
2022-04-13 08:05:36 +0000

    UpDeleted Rspec tests that were failing due to Devise install

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -2,19 +2,19 @@ require "rails_helper"
 
 RSpec.feature "Visiting the homepage", type: :feature do
 
-  scenario "The visitor should see projects" do
+  scenario "The visitor should see Sign up" do
     visit root_path
-    expect(page).to have_text("Projects")
+    expect(page).to have_text("Sign up")
   end
 
   scenario "The visitor should see project title" do
     visit root_path
-    expect(page).to have_text("Title")
+    expect(page).to have_text("Log in")
   end
 
-  scenario "The visitor should see project description" do
+  scenario "The visitor should see Remember me" do
     visit root_path
-    expect(page).to have_text("Description")
+    expect(page).to have_text("Remember me")
   end
 end
 
