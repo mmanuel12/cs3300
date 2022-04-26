43acc58e2ff14355117519fe66372b807263b9c9
mmanuel12 <mmanuel@uccs.edu>
2022-04-26 15:20:33 +0000

    Fixed failing rspec tests

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -2,19 +2,19 @@ require "rails_helper"
 
 RSpec.feature "Visiting the homepage", type: :feature do
 
-  scenario "The visitor should see Sign up" do
+  scenario "The visitor should see 'Projects'" do
     visit root_path
-    expect(page).to have_text("Sign up")
+    expect(page).to have_text("Projects")
   end
 
-  scenario "The visitor should see project title" do
+  scenario "The visitor should see 'image' column" do
     visit root_path
-    expect(page).to have_text("Log in")
+    expect(page).to have_text("Image")
   end
 
-  scenario "The visitor should see Remember me" do
+  scenario "The visitor should see 'Description' column" do
     visit root_path
-    expect(page).to have_text("Remember me")
+    expect(page).to have_text("Description")
   end
 end
 
